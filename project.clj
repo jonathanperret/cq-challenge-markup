a524c61c2236eb5837d2ecd49ff71a8b7d891129
sroccaserra <sroccaserra@yahoo.com>
2010-10-11 22:24:58 +0200

    updated to regular Midje 0.6.1 and renamed test file

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject markup "0.1.0"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [midje "0.6.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]]
+		 [midje "0.6.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
 )
 
