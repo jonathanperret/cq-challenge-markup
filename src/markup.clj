8cc836c7c530d8ece68d710a2308cce6ff024ece
sroccaserra <sroccaserra@yahoo.com>
2010-10-12 20:52:03 +0200

    today's code

--- src/markup.clj
+++ src/markup.clj
@@ -1,12 +1,21 @@
 (ns markup)
 
-(defn join-paragraph-lines [s] `[:p ~(. s replace "\n" " ")])
+(defn header-tag [level]
+  (keyword (str "h" level)))
 
-(defn split-into-paragraphs [s] (. s split "\n\n"))
+(defn process-text-block [block]
+  (let [replaced (. block replace "\n" " ")]
+    (if-let [prefix (re-find #"\*+ " block)]
+      (let [prefix-length (count prefix)
+            tag (header-tag (dec prefix-length))]
+        [tag (. replaced substring prefix-length)])
+      [:p replaced])))
+
+(defn split-into-text-blocks [s]
+  (. s split "\n\n"))
 
 (defn markup [data]
   `(:body
     ~@(if (not (= "" data))
-	(map join-paragraph-lines
-	     (split-into-paragraphs data))
-	)))
+	(map process-text-block
+	     (split-into-text-blocks data)))))
--- test/markup_test.clj
+++ test/markup_test.clj
@@ -23,3 +23,19 @@
   (fact (markup "one paragraph\n\ntwo paragraph") =>
 	[:body [:p "one paragraph"]
 	 [:p "two paragraph"]]))
+
+(deftest one-header
+  (fact (markup "* One header") =>
+    [:body [:h1 "One header"]]))
+
+(deftest a-header-with-a-star-in-the-title
+  (fact (markup "* One header with a * inside") =>
+    [:body [:h1 "One header with a * inside"]]))
+
+(deftest a-level-two-header
+  (fact (markup "** A level two header") =>
+    [:body [:h2 "A level two header"]]))
+
+(deftest a-multiline-header
+  (fact (markup "* A multiline\nheader") =>
+    [:body [:h1 "A multiline header"]]))
